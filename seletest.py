d497285e6771f1af2a96a6367372fe667ef4abc5
STORM0314 <109198971+STORM0314@users.noreply.github.com>
2024-05-03 04:40:50 +0800

    Update seletest.py

--- seletest.py
+++ seletest.py
@@ -13,7 +13,7 @@ driver.get(url)
 
 #第一頁面
 select1 = Select(driver.find_element(By.NAME, 'DropDownList1'))                                            #選擇學年學期
-select1.select_by_value('1052')                                                                            #每個選項的編號為學年+學期,例112年上學期為1121,112年下學期則為1122.
+select1.select_by_value('1122')                                                                            #每個選項的編號為學年+學期,例112年上學期為1121,112年下學期則為1122.
 
 select2 = Select(driver.find_element(By.NAME, 'DropDownList2'))                                            #選擇課程
 select2.select_by_value('1')                                                                               # 1 為"所有課程"之選項代號
@@ -37,15 +37,15 @@ while True:
     for i in row:
         j = i.find_elements(By.TAG_NAME, 'td')
         if len(j)>10:
-            grade_th = j[0].text        #年級
-            class_id = j[2].text        #開課序號
-            sub_id = j[3].text          #科目編號
-            class_nid = j[6].text       #班別
-            ER_id = j[8].text           #選必修
-            credit = j[9].text          #學分
-            #group = j[10].text         #群別
-            sub_name = j[11].text       #科目名稱
-            teacher_name = j[13].text   #教師名稱
+            grade_th = j[0].text                                                                           #年級
+            class_id = j[2].text                                                                           #開課序號
+            sub_id = j[3].text                                                                             #科目編號
+            class_nid = j[6].text                                                                          #班別
+            ER_id = j[8].text                                                                              #選必修
+            credit = j[9].text                                                                             #學分
+            #group = j[10].text                                                                            #群別
+            sub_name = j[11].text                                                                          #科目名稱
+            teacher_name = j[13].text                                                                      #教師名稱
             list = {
                 'Grade_th': grade_th,
                 'Class_ID': class_id,
@@ -57,15 +57,15 @@ while True:
                 'Sub_Name': sub_name,
                 'Teacher':  teacher_name,
             }
-            lists.append(list) # 將新的資料加在原本的資料後面
-    nextpagebut = driver.find_element(By.NAME, 'Button4')#頁面最下方的 '下一頁' 按鈕
-    if nextpagebut.get_attribute('disabled'): #如果'下一頁'按鈕無法互動
+            lists.append(list)                                                                             # 將新的資料加在原本的資料後面
+    nextpagebut = driver.find_element(By.NAME, 'Button4')                                                  #頁面最下方的 '下一頁' 按鈕
+    if nextpagebut.get_attribute('disabled'):                                                              #如果'下一頁'按鈕無法互動
         print('end')
-        with open('d:\engineer\Project\worm\LIST\s1052.csv','a',encoding='utf-8') as f: #開啟檔案並將資料存入
+        with open('d:\engineer\Project\worm\LIST\s1052.csv','a',encoding='utf-8') as f:                    #開啟檔案並將資料存入
             json.dump(lists,f,indent=2,sort_keys=True,ensure_ascii=False)
         break
     else:
-        nextpagebut.click() #按下'下一頁'
+        nextpagebut.click()                                                                                #按下'下一頁'
     
 #for txt in txts:
 #    gradeth = txt.find_element(By.CLASS_NAME) #年級
